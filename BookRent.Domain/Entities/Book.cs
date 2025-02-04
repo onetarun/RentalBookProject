edcb75de6a9fb7799fa0335182ee87c98680e985
onetarun <onetarun@outlook.com>
2025-02-04 11:24:18 +0530

    Key of Book Entity

--- BookRent.Domain/Entities/Book.cs
+++ BookRent.Domain/Entities/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace BookRent.Domain.Entities
 {
     public class Book
     {
+        [Key]
         public int BookID { get; set; }
         public string ISBN { get; set; }
         public string Title { get; set; }
