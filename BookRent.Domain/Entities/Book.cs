cea45a4d580cd6afa489aae8eda0faca70ee5ab2
onetarun <onetarun@outlook.com>
2025-02-04 15:16:35 +0530

    isbn details

--- BookRent.Domain/Entities/Book.cs
+++ BookRent.Domain/Entities/Book.cs
@@ -16,7 +16,7 @@ namespace BookRent.Domain.Entities
 
         //Cancel ok
         public int BookId { get; set; }
-
+        //not isbn
         public string ISBN { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
